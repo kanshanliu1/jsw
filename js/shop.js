4f70643136f533af086760b9d53a4a5a79f40703
kanshanliu1 <wei_6704@qq.com>
2017-08-21 22:56:24 +0800

    完成实时显示最终价格功能

--- js/shop.js
+++ js/shop.js
@@ -1,24 +1,50 @@
 $(function () {
-    var num = 0;
-    $('.addbuycar').click(function () {
-        if(num==0) return ;
-        $(this).css('backgroundColor', '#F81313');
-        var countmoney = 1 * $(this).parent().find('.price').children('.serve-price').text();
-        // alert( countmoney);
-        countmoney *= num;
-        console.log(countmoney);
-        // console.log(countmoney);
-        // console.log(typeof(countmoney))
-        $('.countmoney .price').html(countmoney);
+    // 点击减号事件
+    $('.main').on('click', '.glyphicon-minus', function () {
+        var keynum = +$(this).parent().parent().children().find('.keynum').val();
+        if (keynum <= 0) {
+            keynum == 0;
+            return;
+        };
+        keynum--;
+        $(this).parent().parent().children().find('.keynum').val(keynum);
+        countprice();
+    })
+    // 点击加号事件
+    $('.main').on('click', '.glyphicon-plus', function () {
+        var keynum = +$(this).parent().parent().children().find('.keynum').val();
+        if (keynum >= 99) {
+            keynum == 99;
+            return;
+        };
+        keynum++;
+        $(this).parent().parent().children().find('.keynum').val(keynum);
+        countprice();
+    })
+    // 从购物车删除按钮事件
+    $('.main').on('click', '.delcar', function () {
+        $(this).parent().parent().children().find('.keynum').val(0);
+        countprice();
     });
-    $('.l-content').on('click', '.numedit', function () {
-        if ($(this).hasClass('minus')) {
-            if (num <= 0) num = 0;
-            else num--;
-        } else if ($(this).hasClass('plus')) {
-            if (num >= 99) num = 99;
-            else num++;
+    //键入数量改变事件
+    $('.main').on('blur', '.keynum', function () {
+        if ($(this).val() == '') {
+            $(this).val(0);
         }
-        $(this).parent().find('.num').text(num);
-    });
+        countprice()
+    })
+    // 结算总价格事件
+    function countprice() {
+        var countkeynum = 0;
+        var countprice = 0;
+        $('.keynum').each(function (index, ) {
+            countprice += +($(this).parents().siblings('.price').children('.serve-price').text()) * (+$(this).val());
+            countkeynum += +$(this).val();
+        })
+        // console.log('商品总数量' + countkeynum);
+        // console.log('总价格是' + countprice);
+        $('.countmoney .price').text(countprice);
+        $('.youhuimoney .price').text(countkeynum * 200);
+        $('.actualmoney .price').text(countprice - (countkeynum * 200));
+    }
 })
